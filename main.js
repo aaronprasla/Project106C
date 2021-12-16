610da57680a3d44851c650344ca250bf1aaa5132
aaronprasla <52900151+aaronprasla@users.noreply.github.com>
2021-12-15 19:03:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function start(){
+	navigator.mediaDevices.getUserMedia({audio: true});
+	classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/JgrBM5jHI/model.json', modelReady);
+}
+
+function modelReady() {
+	classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+	if(error){
+		console.error(error);
+	} else {
+		console.log(results);
+
+		r = Math.floor(Math.random() * 255);
+		g = Math.floor(Math.random() * 255);
+		b = Math.floor(Math.random() * 255);
+
+		document.getElementById('result_label').innerHTML = 'I can hear - ' + results[0].label;
+		document.getElementById('result_label').style.color = 'rgb(' + r + ',' + g + ','+ b + ')';
+		document.getElementById('accuracy_label').innerHTML = 'Accuracy - ' + (results[0].confidence * 100).toFixed(2) + '%';
+		document.getElementById('accuracy_label').style.color = 'rgb(' + r + ',' + g + ','+ b + ')';
+
+		img1 = document.getElementById('alien1');
+		img2 = document.getElementById('alien2');
+		img3 = document.getElementById('alien3');
+		img4 = document.getElementById('alien4');
+
+		if(results[0].label == 'Clap'){
+			img1.src = 'aliens-01.gif';
+			img2.src = 'aliens-02.png';
+			img3.src = 'aliens-03.png';
+			img4.src = 'aliens-04.png';
+		} else if(results[0].label == 'Tink'){
+			img1.src = 'aliens-01.png';
+			img2.src = 'aliens-02.gif';
+			img3.src = 'aliens-03.png';
+			img4.src = 'aliens-04.png';
+		} else if(results[0].label == 'Snap'){
+			img1.src = 'aliens-01.png';
+			img2.src = 'aliens-02.png';
+			img3.src = 'aliens-03.gif';
+			img4.src = 'aliens-04.png';
+		} else if(results[0].label == 'Background Noise'){
+			img1.src = 'aliens-01.png';
+			img2.src = 'aliens-02.png';
+			img3.src = 'aliens-03.png';
+			img4.src = 'aliens-04.gif';
+		}
+	}
+}
